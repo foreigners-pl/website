ddefdfa6decb823ac9367204b86994bea581525f
Foreigners.pl <connect@foreigners.pl>
2025-12-07 18:40:13 +0100

    Remove custom cursor - use default browser cursor only

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import CustomCursor from "@/components/ui/CustomCursor";
+// import CustomCursor from "@/components/ui/CustomCursor";
 import CookieConsent from "@/components/CookieConsent";
 import SessionTracker from "@/components/SessionTracker";
 
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased bg-white`}
       >
-        <CustomCursor />
+        {/* <CustomCursor /> */}
         <SessionTracker />
         {children}
         <CookieConsent />
